2b932e71d374415acd84ec56e84ecbd569544a3c
Rachit74 <rachithooda09@gmail.com>
2024-07-19 10:19:56 +0530

    blueprints

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
-
+from .views import *
+from .auth import *
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "QWE123"
+
+    app.register_blueprint(views,url_prefix='/')
+    app.register_blueprint(auth,url_prefix='/auth')
 
     return app
